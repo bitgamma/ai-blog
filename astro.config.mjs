a0bac1a8f6ca31b406acf500db54fac272ad45e6
Michele Balistreri <michele@bitgamma.com>
2026-02-27 20:21:46 +0100

    fix urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://bitgamma.github.io',
-	base: '/ai-blog',
+	base: '/ai-blog/',
+	trailingSlash: 'always',
 	integrations: [mdx(), sitemap()],
 });
