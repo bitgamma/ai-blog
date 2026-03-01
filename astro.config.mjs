fa58d06b4ec551b5f7f5a890979b242100166b5c
Ksenia Lebedeva <ksenia@bitgamma.com>
2026-03-01 13:44:06 +0100

    added style

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,7 @@ export default defineConfig({
 	base: '/ai-blog/',
 	trailingSlash: 'always',
 	integrations: [mdx(), sitemap()],
+  redirects: {
+    '/': 'blog/'
+  }
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Michele Balistreri\'s notes on AI';
+export const SITE_TITLE = 'Michele\'s notes on AI';
 export const SITE_DESCRIPTION = 'Notes, guide and thought about AI-related stuff';
